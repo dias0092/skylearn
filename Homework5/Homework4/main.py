2e810c103d8cc0b7dd350cfcf597d8e6e8a2e548
Dias Mukhametarkhim <54438346+dias0092@users.noreply.github.com>
2023-05-11 06:46:37 +0000

    refactor main chhange functions to lambda

--- Homework5/Homework4/main.py
+++ Homework5/Homework4/main.py
@@ -2,8 +2,14 @@
 #Password: root
 
 from colorama import init, Fore, Style
-from user_functions import load_user_data , save_user_data , login , register_new_user 
-from user_actions import get_user_balance, get_user_input , deposit_options , transfer , top_up_main_balance , get_user_transaction_history, handle_deposit , withdraw_main_balance, display_balance , display_transaction_history
+from user_functions import (
+    load_user_data, save_user_data, login, register_new_user
+)
+from user_actions import (
+    get_user_balance, get_user_input, deposit_options, transfer, 
+    top_up_main_balance, get_user_transaction_history, handle_deposit, 
+    withdraw_main_balance, display_balance, display_transaction_history
+)
 
 # Initialize the colorama library
 init()
@@ -13,63 +19,44 @@ print(f"{Fore.CYAN}{Style.BRIGHT}Welcome to the Bank of Jusan!{Style.RESET_ALL}"
 print(f"{Fore.RED}Attention! Please use only numeric value when you write your answer{Style.RESET_ALL}")
 
 def main():
-  # Initialize user data
-  user_data = load_user_data()
-  # Initialize user session
-  user_session = None
-  while True:
-    if user_session is not None:
-      user_session = logged_in_menu(user_data, user_session)
-    else:
-      user_session = logged_out_menu(user_data)
-
-  save_user_data(user_data)
-
-
+    user_data = load_user_data()
+    user_session = None
+    
+    while True:
+        user_session = logged_in_menu(user_data, user_session) if user_session else logged_out_menu(user_data)
+    
+    save_user_data(user_data)
+  
 # Funtion to choose in main menu
 def logged_in_menu(user_data, user_session):
-  print_main_menu()
-  
-  user_choice = get_user_input("Choose an action: ", int)
-  balance = get_user_balance(user_data, user_session)
+    print_main_menu()
+    user_choice = get_user_input("Choose an action: ", int)
+    balance = get_user_balance(user_data, user_session)
 
-  if user_choice == 1:  # Deposit
-    
-    options = deposit_options()
-    transaction_history = get_user_transaction_history(user_data, user_session)
+    actions = {
+        1: lambda: handle_deposit(deposit_options(), get_user_transaction_history(user_data, user_session), balance),  # Deposit
+        2: lambda: top_up_main_balance(user_data, user_session), # Top up main balance
+        3: lambda: withdraw_main_balance(user_data, user_session), # Withdraw from main balance
+        4: lambda: display_balance(balance), # Display Balance
+        5: lambda: display_transaction_history(user_data, user_session), # Display History
+        6: lambda: transfer(user_data, user_session), # Transfer
+        7: lambda: None  # Log out
+    }
   
-    handle_deposit(options, transaction_history, balance)
-  elif user_choice == 2:  # Top up main balance
-    top_up_main_balance(user_data, user_session)
-  elif user_choice == 3:  # Withdraw from main balance
-    withdraw_main_balance(user_data, user_session)
-  elif user_choice == 4:  # Display balance
-    display_balance(balance)
-  elif user_choice == 5:  # Transaction history
-    display_transaction_history(user_data, user_session)
-  elif user_choice == 6:  # Transfer
-    transfer(user_data, user_session)
-  elif user_choice == 7:  # Log out
-    return None
+    return actions.get(user_choice, lambda: user_session)()
   
-  return user_session
-
-
 # Funtion to choose in login menu
 def logged_out_menu(user_data):
-  print_login_menu()
+    print_login_menu()
 
-  user_choice = get_user_input("Choose an action: ", int)
-  if user_choice == 1:  # Login
-    return login(user_data)
-  elif user_choice == 2:  # Register
-    register_new_user(user_data)
-  elif user_choice == 3:  # Exit
-    exit(0)
+    actions = {
+        1: lambda: login(user_data),
+        2: lambda: register_new_user(user_data),
+        3: lambda: exit(0)
+    }
+    
+    return actions.get(get_user_input("Choose an action: ", int), lambda: None)()
 
-  return None
-  
-# Function to print the main menu
 def print_main_menu():
     print("\nMain Menu\n")
     print("1. Deposit")
@@ -80,7 +67,6 @@ def print_main_menu():
     print("6. Transfer")
     print("7. Log out\n")
 
-# Function to print the login menu
 def print_login_menu():
     print("\n1. Login")
     print("2. Register")
@@ -88,6 +74,3 @@ def print_login_menu():
   
 if __name__ == "__main__":
     main()
-
-
-
